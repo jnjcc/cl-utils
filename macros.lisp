19611e89ec0b002aba37dcc09383470874bc80f9
jnjcc <jnjcc@live.com>
2014-08-05 21:35:41 +1200

    macro ONCE-ONLY from Practical Common Lisp
    ONCE-ONLY is used to generate code that evaluates certain
    macro arguments once only and in a particular order.

---
+++ macros.lisp
@@ -0,0 +1,8 @@
+(defmacro once-only-pcl ((&rest names) &body body)
+  "Used to generate code that evaluates certain macro arguments
+once only and in a particular order"
+  (let ((gensyms (loop for n in names collect (gensym))))
+    `(let (,@(loop for g in gensyms collect `(,g (gensym))))
+       `(let (,,@(loop for g in gensyms for n in names collect ``(,,g ,,n)))
+          ,(let (,@(loop for n in names for g in gensyms collect `(,n ,g)))
+                ,@body)))))
---
+++ test/macros.lisp
@@ -0,0 +1,14 @@
+(load "../macros.lisp")
+
+(defun primep (number)
+  (when (> number 1)
+    (loop for fac from 2 to (isqrt number) never (zerop (mod number fac)))))
+
+(defun next-prime (number)
+  (loop for n from number when (primep n) return n))
+
+(defmacro do-primes ((var start end) &body body)
+  (once-only-pcl (start end)
+    `(do ((,var (next-prime ,start) (next-prime (1+ ,var))))
+         ((> ,var ,end))
+       ,@body)))
