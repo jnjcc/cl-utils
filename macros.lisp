71b8ee3a43b573745e3d3a518d34999c523cb181
jnjcc <jnjcc@live.com>
2014-08-05 21:53:22 +1200

    ONCE-ONLY from SBCL
    ONCE-ONLY is a utility useful in writing source transforms and
    macros. It provides a concise way to wrap a LET around some code
    to ensure that some forms are only evaluated once.

--- macros.lisp
+++ macros.lisp
@@ -1,3 +1,4 @@
+;;; ONCE-ONLY from Practical Common Lisp
 (defmacro once-only-pcl ((&rest names) &body body)
   "Used to generate code that evaluates certain macro arguments
 once only and in a particular order"
@@ -6,3 +7,22 @@ once only and in a particular order"
        `(let (,,@(loop for g in gensyms for n in names collect ``(,,g ,,n)))
           ,(let (,@(loop for n in names for g in gensyms collect `(,n ,g)))
                 ,@body)))))
+
+;;; modified NAMED-LET from SBCL
+(defmacro named-let (name binds &body body)
+  `(labels ((,name ,(mapcar #'first binds) ,@body))
+     (,name ,@(mapcar #'second binds))))
+;;; modified ONCE-ONLY from SBCL
+(defmacro once-only (specs &body body)
+  (named-let frob ((specs specs)
+                   (body body))
+    (if (null specs)
+        `(progn ,@body)
+        (let ((spec (first specs)))
+          (let* ((name (first spec))
+                 (exp-temp (gensym "ONCE-ONLY")))
+            `(let ((,exp-temp ,(second spec))
+                   (,name (gensym ,(symbol-name name))))
+               `(let ((,,name ,,exp-temp))
+                  ,,(frob (rest specs) body))))))))
+
--- test/macros.lisp
+++ test/macros.lisp
@@ -7,8 +7,16 @@
 (defun next-prime (number)
   (loop for n from number when (primep n) return n))
 
-(defmacro do-primes ((var start end) &body body)
+(defmacro do-primes-pcl ((var start end) &body body)
   (once-only-pcl (start end)
     `(do ((,var (next-prime ,start) (next-prime (1+ ,var))))
          ((> ,var ,end))
        ,@body)))
+
+(defmacro do-primes ((var start end) &body body)
+  (once-only ((start start)
+              (end end))
+    `(do ((,var (next-prime ,start) (next-prime (1+ ,var))))
+         ((> ,var ,end))
+       ,@body)))
+
